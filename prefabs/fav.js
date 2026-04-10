64a8e7a6b095478f2e3a1ff76ab62d7869b5b76e
Groupe Nora <noragroup.magistrade@gmail.com>
2026-04-10 13:39:22 +0200

    Add files via upload

--- prefabs/fav.js
+++ prefabs/fav.js
@@ -1,9 +1,14 @@
 class FavorisButton extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <button class="favoris-btn">❤️ Favoris</button>
+    connectedCallback() {
+        this.innerHTML = `
+    <button class="bookmarkBtn" onclick="addToFavorites('https://flcorp.github.io/flcflix/films/filmspages/action/argylle.html', 'https://github.com/FLCorp/flcflixposters/blob/main/argylle.jpg?raw=true')">
+        <span class="IconContainer">
+             <i class="fi fi-sr-bookmark"></i>
+        </span>
+        <p class="text">Favoris</p>
+    </button>
     `;
-  }
+    }
 }
 
 customElements.define("favoris-button", FavorisButton);
